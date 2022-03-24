4a42e247418ac978ea35dfc505f14758f1c1878b
ronphobos <sidibearouna71@gmail.com>
2022-03-24 17:07:56 +0000

    debugage

--- resources/views/admin/projet/taches/index.blade.php
+++ resources/views/admin/projet/taches/index.blade.php
@@ -138,7 +138,7 @@
                                 <td class="a-center ">
                                 <input type="checkbox" class="flat" name="table_records">
                                 </td>
-                                <td class=" ">{{ $projet->tache_id }}</td>
+                                <td class=" ">{{ $projet->id }}</td>
                                 <td class=" ">May 23, 2014 11:47:56 PM </td>
                                 <td class=" ">121000210 <i class="success fa fa-long-arrow-up"></i></td>
                                 <td class=" ">John Blank L</td>
@@ -149,7 +149,7 @@
                             </tr>
                         @else
                             <tr>
-                                <td colspan="9" style="text-align: center;">Il n'ya pas encore de tâche</td>
+                                <td colspan="9" style="text-align: center;">Il n'ya pas encore de tâche {{ $projet->taches->count() }}</td>
                             </tr>
                         @endif
 
@@ -167,12 +167,7 @@
         <!-- /page content -->
 
         <!-- footer content -->
-        <footer>
-          <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
+       @include('admin.layout.footer')
         <!-- /footer content -->
       </div>
     </div>
