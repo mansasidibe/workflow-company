d3ab7f99b02651391e44eb494a200047c3c9501f
bim group <bimgroup225@gmail.com>
2022-06-16 21:54:12 +0000

    paufinage

--- resources/views/chef-equipe/projet/index.blade.php
+++ resources/views/chef-equipe/projet/index.blade.php
@@ -130,7 +130,7 @@
                           <th style="width: 20%">Action</th>
                         </tr>
                       </thead>
-                      <tbody>
+                      {{-- <tbody>
                         @if ($taches->count())
                             @foreach ($taches as $projet)
 
@@ -178,7 +178,7 @@
                           </td>
                         </tr>
 
-                        {{-- modal ici --}}
+
                         <div class="modal fade bs3-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
                             <div class="modal-dialog modal-lg">
                             <div class="modal-content">
@@ -241,7 +241,132 @@
                             </div>
                             </div>
                         </div>
-                        {{-- fin model --}}
+
+
+                         @endforeach
+                          @else
+                          <tr>
+                              <td style="text-align: center" colspan="8">Pas de projet</td>
+                          </tr>
+
+                          @endif
+
+                      </tbody>
+                    </table> --}}
+                    <!-- end project list -->
+
+                     <tbody>
+                        @if ($projets->count())
+                            @foreach ($projets as $projet)
+
+                        <tr>
+                            <input type="hidden" class="btn-suppres" value="{{ $projet->id }}">
+                          <td>{{ $projet->id }}</td>
+                          <td>
+                            <a>{{ $projet->nom }}</a>
+                            <br />
+                            <small>Créé le {{ $projet->created_at }}</small>
+                            <br />
+                            <small>Modifié : {{ $projet->updated_at->diffForHumans() }} par : admin </small>
+                          </td>
+                          <td> {{ $projet->duree }} </td>
+                          <td class="project_progress">
+                            <div class="progress progress_sm">
+                              <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="
+                               @foreach ($taches as $key => $value)  
+                                    @if($value->projet_id == $projet->id && $taches_tota > 0)
+                                        {{ ($projet->id * 100)/$taches_tota }}
+                                    @endif
+                                @endforeach">
+                              </div>
+                            </div>
+                            <small>
+
+                                @foreach ($taches as $key => $value)
+                                    @if($value->projet_id == $projet->id && $taches_tota > 0)
+                                        {{ ceil(($projet->id * 100)/$taches_tota)}}.
+                                    @endif
+                                @endforeach
+                                % Completé</small>
+                          </td>
+                           <td>
+                            @if ( $projet->etat == "encours")
+                                <button type="button" class="btn btn-warning btn-xs">En cours</button>
+                            @elseif ( $projet->etat == "termine")
+                                <button type="button" class="btn btn-success btn-xs">Terminé</button>
+                            @else
+                                <button type="button" class="btn btn-danger btn-xs">Début</button>
+                            @endif
+                          </td>
+                          <td>
+                            <a href="{{ route('equipe.tache', $projet->id ) }}"  class="btn btn-primary btn-xs"><i class="fa fa-folder"></i> Voir </a>
+                          </td>
+                        </tr>
+
+
+                        <div class="modal fade bs3-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+
+                                <form class="form-horizontal form-label-left" enctype="multipart/form-data" method="POST" action="{{ route('projets.update', $projet->id) }}">
+                                    @method('PUT')
+                                    @csrf
+                                    <br>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Nom du projet</label>
+                                        <div class="col-md-9 col-sm-9 col-xs-12">
+                                            <input type="text" name="nom" value="{{ $projet->nom }}" class="form-control" >
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Date de début</label>
+                                        <div class="col-md-9 col-sm-9 col-xs-12">
+                                            <input type="date" name="date_debut" value="{{ $projet->date_debut }}" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Durée</label>
+                                        <div class="col-md-9 col-sm-9 col-xs-12">
+                                            <input type="text" name="duree" value="{{ $projet->duree }}" class="form-control" >
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Assigner une équipe</label>
+                                        <div class="col-md-9 col-sm-9 col-xs-12">
+                                        <select name="equipe_id" class="form-control">
+                                            @if ($equipes->count())
+                                                @foreach ($equipes as $equipe)
+                                                    <option value="{{ $equipe->id }}">{{ $equipe->nom }}</option>
+                                                @endforeach
+                                            @else
+                                            <option value="">Pas d'équipe</option>
+                                            @endif
+                                        </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Assigner une équipe</label>
+                                        <div class="col-md-9 col-sm-9 col-xs-12">
+                                        <select name="etat" class="form-control">
+                                            <option value="debut">Début</option>
+                                            <option value="encours">En cours</option>
+                                            <option value="termine">Terminé</option>
+                                        </select>
+                                        </div>
+                                    </div>
+
+                                    <br>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+                                        <button type="submit" class="btn btn-primary">Ajouter</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                            </div>
+                        </div>
+
 
                          @endforeach
                           @else
@@ -253,7 +378,6 @@
 
                       </tbody>
                     </table>
-                    <!-- end project list -->
 
                   </div>
                 </div>
